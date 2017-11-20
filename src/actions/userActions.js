4cdfdcf556125314a85514eba3a44944ca73f4a2
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-19 18:59:57 -0800

    Moved api code to its own file.

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,18 +1,7 @@
 import '../polyfill';
 import VeritoneApi from 'veritone-client-js/dist/bundle-browser';
 import * as types from '../constants/actionTypes';
-
-import Cookies from 'universal-cookie';
-
-const cookies = new Cookies();
-const oauthToken = cookies.get('oauthToken');
-let client;
-if (oauthToken) {
-    client = VeritoneApi({
-        oauthToken: oauthToken,
-        baseUrl: 'http://local.veritone.com:9000'
-    });
-}
+import { getCurrentUser } from '../api/api';
 
 export const getUserStart = () => {
     return {
@@ -37,15 +26,8 @@ export const getUserFailure = (error) => {
 };
 
 export const getUser = () => (dispatch) => {
-    let query = `query {
-        me {
-            name,
-            id,
-            jsondata
-        }
-    }`;
     dispatch(getUserStart());
-    return client.graphql.query(query)
+    return getCurrentUser()
         .then(response => {
             dispatch(getUserSuccess(response.data.me));
         }, error => {
